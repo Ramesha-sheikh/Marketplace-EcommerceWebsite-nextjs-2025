627cdd099f9f54c2ebde1ace88efad52207f298d
Ramesha javed <165018589+Ramesha-sheikh@users.noreply.github.com>
2025-02-08 15:14:46 +0500

    Your commit message

--- src/Components/Cardsidebar/Single product/Carddeatils.tsx
+++ src/Components/Cardsidebar/Single product/Carddeatils.tsx
@@ -165,6 +165,8 @@ const ProductPage = () => {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
+          "Authorization": `Bearer ${process.env.NEXT_PUBLIC_API_SECRET_KEY}`, // Use dynamic API key
+
         },
         body: JSON.stringify({
           productId: product.id,
